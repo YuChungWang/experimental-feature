3ae3e1c77d5329beb39a3174a5d67765246f8bd5
Charlie Wang <charliewang324@gmail.com>
2023-02-21 11:13:28 +0800

    ADD: Reactivity transform

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="handleBtnAddClick">ADD</button>
+  <p>{{ `Parent: ${cp}` }}</p>
+  <Child />
+</template>
+
+<script setup>
+import Child from './components/Child.vue';
+import { ref } from 'vue';
+  
+let cp = ref(0);
+
+const handleBtnAddClick = () => {
+  cp.value += 1;
+}
+</script>
---
+++ src/components/Child.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>{{ `Child: ${props.cp}` }}</p>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<{ cp: number }>(), {cp: 1});
+
+// Reactive props destructure
+// const { cp = 1 } = defineProps<{ cp: number }>();
+</script>
